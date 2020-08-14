ab1538f076e659d3c0444d91c4b3a06aa7a95ae1
unknown <marcos.devel@gmail.com>
2020-08-14 20:30:36 -0300

    update-youtube-fake

---
+++ src/pages/YTFake/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import * as Redux from 'react-redux';
+// import { View, Text } from 'react-native';
+
+// import styles from './style';
+
+const YTFake: React.FC = (props) => {
+  // return (<View><Text>Olá Mundo!</Text></View>);
+  return <div>Olá Mundo!</div>;
+};
+
+export default YTFake;
