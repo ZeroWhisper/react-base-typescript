dede8719922c4e9082105ccd587e98fdf1fecb28
unknown <marcos.devel@gmail.com>
2020-09-02 22:27:19 -0300

    update-style

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -9,3 +9,7 @@ export const css_center = css`
 export const css_box_shadow = css`
   box-shadow: 0 1px 4px 0 rgba(255, 255, 255, 0.5);
 `;
+
+export const css_box_shadow_blue = css`
+  box-shadow: rgb(0, 115, 183) 0px 0px 6px;
+`;
