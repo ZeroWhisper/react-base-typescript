3ec1b86a7468b7347bb41da589d2da9db1035f37
unknown <marcos.devel@gmail.com>
2020-09-02 23:07:34 -0300

    super-stash-myself

---
+++ src/services/stash/index.js
@@ -0,0 +1,19 @@
+import lz from 'lz-string';
+
+export const stash = v => {
+  const level_one = lz.compress(JSON.stringify(v));
+  const level_two = lz.compressToBase64(level_one);
+  return level_two;
+};
+
+export const unstash = v => {
+  const level_two = lz.decompressFromBase64(v);
+  const level_one = lz.decompress(level_two);
+  return JSON.parse(level_one);
+};
+
+// const c = lz.compress(JSON.stringify(firebaseConfig));
+
+// console.log('Compress', c);
+
+// const d = JSON.parse(lz.decompress(c));
